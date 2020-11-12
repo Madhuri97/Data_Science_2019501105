ccec11b643c23018644d15ba4fb516a9d904d50d
Madhuri97 <madhurinaidu1997@msitprogram.net>
2020-11-12 20:33:18 +0530

    Assignment -5

--- Data Mining/DM Assignment5/Question14.R
+++ Data Mining/DM Assignment5/Question14.R
@@ -12,4 +12,4 @@ print(iqr)
 exams[(exams$Midterm.2 > q3 + 1.5 * iqr), 3]
 exams[(exams$Midterm.2 > q1 - 1.5 * iqr), 3]
 
-boxplot(exams$Midterm.2, col = "lightblue", main = "Exam Scores", xlab = ("Exam 2"), ylab = "Exam Score")
+boxplot(exams$Midterm.1,exams$Midterm.2, col = "lightblue", main = "Exam Scores", names = c("Exam1","Exam 2"), ylab = "Exam Score")
